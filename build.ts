aaaea6c4c822747cabe811d901ac781ab7dc59fe
apple-yagi <applesryu@gmail.com>
2022-11-05 12:26:42 +0900

    Setup build script
    - ref: https://zenn.dev/rabbit/articles/ef84ae02a987b2

---
+++ build.ts
@@ -0,0 +1,25 @@
+import { build } from "esbuild";
+
+const entryFile = "src/index.ts";
+const shared = {
+  bundle: true,
+  entryPoints: [entryFile],
+  minify: true,
+  sourcemap: true,
+};
+
+build({
+  ...shared,
+  format: "esm",
+  outfile: "./dist/index.esm.js",
+  target: ["ES6"],
+  logLevel: "info",
+});
+
+build({
+  ...shared,
+  format: "cjs",
+  outfile: "./dist/index.cjs.js",
+  target: ["ES6"],
+  logLevel: "info",
+});
